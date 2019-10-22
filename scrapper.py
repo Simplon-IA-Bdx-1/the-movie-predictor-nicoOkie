64851572f2a33d2f751ca1caf4361ee94334760a
nicookie <chapon.nicola@gmail.com>
2019-10-22 12:33:22 +0200

    get fr title

--- scrapper.py
+++ scrapper.py
@@ -6,21 +6,7 @@ import bs4
 page = requests.get('https://fr.wikipedia.org/wiki/Django_Unchained')
 
 soup = bs4.BeautifulSoup(page.content, 'html.parser')
-html = list(soup.children)[2]
-body = list(html.children)[3]
 
-def getTextNode(param='', string=''):
-    return body.find(param).get_text()
-
-title = getTextNode('h1')
-# print(title)
-
-technicalSheetId = body.find(id='Fiche_technique')
-for parent in technicalSheetId.parents:
-    if parent.name == 'h2':
-        technicalSheetH2 = parent
-
-technicalSheetList = technicalSheetH2.next_sibling.next_sibling
-
-print(technicalSheetList)
+def get_title():
+    return soup.find('h1').get_text()
 
